b652707e21ce5bc2d72bf492a82b82e83bdf1fab
Mohammed Alshaer <momo.alshaer@hord.app>
2024-04-04 16:55:37 +0200

    add speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Layout/Header/Header";
 import Connectors from "./components/Connectors/Connectors";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import "./App.scss";
 
 function App() {
   return (
     <div className="flex flex-col justify-center">
+      <SpeedInsights />
       <Header />
       <Connectors />
     </div>
