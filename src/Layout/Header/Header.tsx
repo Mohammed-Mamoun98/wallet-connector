fbb217cf1a4bd9ccc4c214971a60237361411e3c
Mohammed Alshaer <momo.alshaer@hord.app>
2024-05-01 12:39:43 +0300

    include connectedContent in connectbtn

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { useWalletStore } from "../../state/stores/walletState";
 import ConnectBtn from "../../components/ConnectBtn/ConnectBtn";
-import ChainSelector from "src/components/ChainSelector/ChainSelector";
-import ConnectedContent from "src/components/ConnectedContent/ConnectedContent";
-import Modal from "src/components/Shared/Modal/Modal";
 
 export default function Header() {
   return (
     <div className="py-4 flex px-2 gap-2 bg-[#2c2c2c] justify-between items-center">
       <ConnectBtn />
-      <ConnectedContent />
     </div>
   );
 }
--- src/components/ConnectBtn/ConnectBtn.tsx
+++ src/components/ConnectBtn/ConnectBtn.tsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useWalletStore } from "../../state/stores/walletState";
 import { useModalStore } from "src/state/stores/modalState";
 import ConnectWalletModal from "./ConnectWalletModal/ConnectWalletModal";
+import { retrieveFromLs } from "src/utils/localstorage";
+import { WALLET_CONNECTION_TYPE } from "src/services/wallet/connectionPreservers";
+import { connectorsList } from "src/constants/connectors/list";
+import useWalletConnection from "src/hooks/useWalletConnection/useWalletConnection";
+import ConnectedContent from "../ConnectedContent/ConnectedContent";
+
+const storedConnectioType = retrieveFromLs(WALLET_CONNECTION_TYPE);
+const uiConnector = connectorsList.find(
+  (con) => con.connector.name === storedConnectioType
+);
+
+const defaultConnector = uiConnector ? uiConnector.connector : null;
 
 export default function ConnectBtn() {
   const { account } = useWalletStore();
   const { openModal } = useModalStore();
+  const [{ hanldeConnection }] = useWalletConnection();
 
   const openConnectionModal = () => {
     openModal({ modalContent: <ConnectWalletModal /> });
   };
 
-  if (account) return <></>;
+  useEffect(() => {
+    if (!defaultConnector) return;
+    hanldeConnection(defaultConnector);
+  }, []);
+
+  if (account) return <ConnectedContent />;
 
   return (
     <div
       onClick={openConnectionModal}
-      className="text-white ml-auto bg-[#17BD3B] hover:bg-[#3CE461] p-2 rounded-[12px] cursor-pointer"
+      className="text-white bg-[#6140ef] hover:bg-[#4e33bf] p-2 rounded-[12px] cursor-pointer font-mono text-center w-full md:w-fit"
     >
-      Connect
+      Connect Wallet
     </div>
   );
 }
