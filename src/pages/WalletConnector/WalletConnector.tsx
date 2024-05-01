4e065d3e27ab92f93d19133afbb4340840c808ca
Mohammed Alshaer <momo.alshaer@hord.app>
2024-05-01 13:15:46 +0300

    playground file

--- src/pages/WalletConnector/WalletConnector.tsx
+++ src/pages/WalletConnector/WalletConnector.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "src/Layout/Header/Header";
+import { usePromise } from "src/hooks/usePromise/usePromise";
+
+interface IContractResponse {
+  value: number;
+}
+
+const getContractValue = () =>
+  new Promise<IContractResponse>((res, rej) => {
+    setTimeout(() => {
+      res({ value: 2000 });
+    }, 2000);
+  });
+
+type A = Awaited<ReturnType<typeof getContractValue>>;
 
 export default function WalletConnector() {
+  const [callGetContract, contractValue, loading, error] =
+    usePromise(getContractValue);
+
+  useEffect(() => {
+    console.log({ contractValue });
+  }, [contractValue?.value]);
   return (
     <div>
       <Header />
+      {JSON.stringify({ contractValue, loading, error })}
       WalletConnector
+      <button onClick={callGetContract}>call</button>
     </div>
   );
 }
