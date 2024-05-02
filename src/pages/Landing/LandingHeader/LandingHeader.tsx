ed2b5f8b4756c71ff10765a6eba0b6b0bedf8eb8
Mohammed Alshaer <momo.alshaer@hord.app>
2024-05-02 20:17:03 +0300

    fix name in landingHeader

--- src/pages/Landing/LandingHeader/LandingHeader.tsx
+++ src/pages/Landing/LandingHeader/LandingHeader.tsx
@@ -8,7 +8,7 @@ export default function LandingHeader() {
     <div className="">
       <div className="flex justify-between items-center flex-wrap flex-col-reverse md:flex-row mb-0 md:mb-7">
         <span className="md:text-[36px] text-[26px] font-semibold  mt-7 md:mt-0">
-          Mohammed AIshaer
+          Mohammed Alshaer
         </span>
         <div className="flex justify-center md:justify-start w-full md:w-fit">
           <Suspense fallback={<></>}>
